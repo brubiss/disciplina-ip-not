913e815243ab9082b0a5b040047c83e15e424560
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-02-25 21:59:09 -0300

    Resolvido enunciado B.

--- unidade_1/EnunciadoA.java
+++ unidade_1/EnunciadoA.java
@@ -3,9 +3,9 @@ public class EnunciadoA {
   public static void main(String[] args) {
 
     // Entrada
-    int nota1 = 7;
-    int nota2 = 8;
-    int nota3 = 9;
+    int nota1 = 4;
+    int nota2 = 6;
+    int nota3 = 5;
 
     // Processo
     int media = (nota1 + nota2 + nota3) / 3;
---
+++ unidade_1/EnunciadoB.java
@@ -0,0 +1,15 @@
+public class EnunciadoB {
+  public static void main(String[] args) {
+
+    // Entrada
+    int largura = 3; 
+    int altura = 90;
+
+    //  Processo
+    int area = largura * altura;
+
+    // Saída
+    System.out.println("Área: "+area);
+    
+  }
+}
